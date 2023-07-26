83d91378f681a2e88daab8703961f2b217f79114
b1908306phong <khanhhuy1810k@gmail.com>
2023-07-26 22:13:26 +0700

    26/7/2

--- src/Component/Layout/Search/index.jsx
+++ src/Component/Layout/Search/index.jsx
@@ -12,62 +12,77 @@ function Search() {
     const [searchValue, setSearchValue] = useState('')
     const [searchResult, setSearchResult] = useState([])
     const [showResult, setShowResult] = useState(true)
+    const [loading, setLoading] = useState(false)
     const inputRef = useRef()
     useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1, 2, 3])
-        }, 0)
-    }, [])
+        if (!searchValue.trim()) {
+            setSearchResult([])
+            return
+        }
+        setLoading(true)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+            .then(res => res.json())
+            .then((res) => {
+                setSearchResult(res.data)
+                setLoading(false)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }, [searchValue])
     const handleClear = () => {
+        setSearchResult([''])
         setSearchValue('')
         inputRef.current.focus()
     }
     const handleHideResult = () => {
         setShowResult(false)
     }
-    return (<HeadLessTippy
-        interactive
-        visible={showResult && searchResult.length > 0}
-        render={attrs => (
-            <div className={cx('search-result')} tabIndex='-1'{...attrs}>
-                <PopperWappper>
-                    <h4 className={cx('search-title')}>
-                        Accounts
-                    </h4>
-                    <SearchAcount />
-                    <SearchAcount />
-                    <SearchAcount />
+    return (
+        <HeadLessTippy
+            interactive
+            visible={showResult && searchResult.length > 0}
+            render={attrs => (
+                <div className={cx('search-result')} tabIndex='-1'{...attrs}>
+                    <PopperWappper>
+                        <h4 className={cx('search-title')}>
+                            Accounts
+                        </h4>
+                        {searchResult.map((result) => (
+                            <SearchAcount key={result.id} data={result} />)
 
-                </PopperWappper>
-            </div>
+                        )}
 
-        )}
-        onClickOutside={handleHideResult}
-    >
-        <div className={cx('search')}>
-            <input
-                ref={inputRef}
-                placeholder="Tìm Kiếm"
-                value={searchValue}
-                spellCheck={false}
-                onChange={(e) => setSearchValue(e.target.value)}
-                onFocus={() => setShowResult(true)}
-            />
-            {!!searchValue && (
-                < button className={cx('clear')} onClick={handleClear}>
+                    </PopperWappper>
+                </div>
 
-                    <FontAwesomeIcon icon={faCircleXmark} />
-                </button>
             )}
+            onClickOutside={handleHideResult}
+        >
+            <div className={cx('search')}>
+                <input
+                    ref={inputRef}
+                    placeholder="Tìm Kiếm"
+                    value={searchValue}
+                    spellCheck={false}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    onFocus={() => setShowResult(true)}
+                />
+                {!!searchValue && (
+                    < button className={cx('clear')} onClick={handleClear}>
 
-            {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                )}
+                {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
 
 
-            <button className={cx('search-btn')}>
-                <SearchIcon />
-            </button>
-        </div>
-    </HeadLessTippy >);
+
+                <button className={cx('search-btn')}>
+                    <SearchIcon />
+                </button>
+            </div>
+        </HeadLessTippy >);
 }
 
 export default Search;
--- src/Component/Layout/components/Header/SearchAcount/index.jsx
+++ src/Component/Layout/components/Header/SearchAcount/index.jsx
@@ -1,22 +1,23 @@
 import classNames from "classnames/bind";
 import styles from './SearchAcount.module.scss'
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import Images from "~/Component/Images";
 const cx = classNames.bind(styles)
 
-function SearchAcount() {
+function SearchAcount({ data }) {
     return (
-        <div className={cx("wrapper")}>
-            <Images className={cx("avatar")} src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-aiso/32c9ce145841da2605f79eb64291180b~c5_100x100.jpeg?biz_tag=tiktok_user.user_cover&x-expires=1689753600&x-signature=Abykr9d6%2BGHxykiQlg%2FDL4OSqzo%3D" alt="Thảo" />
+        <Link to={`/@${data.nickname}`} className={cx("wrapper")}>
+            <Images className={cx("avatar")} src={data.avatar} alt={data.full_name} />
             <div className={cx("info")}>
                 <h4 className={cx("name")} >
-                    <span>Nguyễn Thị Thảo Nguyên</span>
-                    <FontAwesomeIcon className={cx("check")} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && < FontAwesomeIcon className={cx("check")} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx("username")} >thaonguyen19308</span>
+                <span className={cx("username")} >{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
